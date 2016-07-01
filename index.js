9e868976090c792cd8b8b386ff4d9c0abd5d7321
Chas Ballew <chas@aptible.com>
2016-07-01 08:19:57 -0600

    Add attr selection

--- index.js
+++ index.js
@@ -10,10 +10,30 @@ module.exports = {
 
         // Given an array of hashes, return an array of hashes where the
         // specified key/value match is found
+
+        // filter_by_key_value([{"id": "chas"}, {"id": "frank"}], "id", "chas");
+        // > [{"id": "chas"}]
+
         filter_by_key_value: function(arr, key, value) {
             return arr.filter(function (a) {
                 return a[key] == value;
             });
+        },
+
+        // Given an array of hashes, a unique id, and an attribute, return the
+        // attribute for the hash where the id matches
+
+        // select_attr_via_id([
+        //    {"id": "chas", "email":"chas@aptible.com"}, 
+        //    {"id": "frank", "email":"frank@aptible.com"}], 
+        // "chas", "email");
+        // > "chas@aptible.com"
+
+        select_attr_via_id: function(arr, id, attr) {
+            target = arr.filter(function (a) {
+                return a['id'] == id;
+            });
+            return target[0][attr];
         }
     }
 };
