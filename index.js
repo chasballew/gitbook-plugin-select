8ee590a0acbf3b26e8c2ce94d1e2ab477eeda7d7
Chas Ballew <chas@aptible.com>
2016-07-09 15:04:25 -0700

    Add select_users

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ module.exports = {
         // Given an array of hashes, a unique id, and an attribute, return the
         // attribute for the hash where the id matches
 
-        // select_attr_via_id([
-        //    {"id": "chas", "email":"chas@aptible.com"}, 
-        //    {"id": "frank", "email":"frank@aptible.com"}], 
-        // "chas", "email");
+        // select_attr_via_id(
+        //     [
+        //         {"id": "chas", "email":"chas@aptible.com"}, 
+        //         {"id": "frank", "email":"frank@aptible.com"}
+        //     ], 
+        //     "chas", 
+        //     "email"
+        // );
         // > "chas@aptible.com"
 
         select_attr_via_id: function(arr, id, attr) {
@@ -34,6 +38,36 @@ module.exports = {
                 return a['id'] == id;
             });
             return target[0][attr];
+        },
+
+        // Takes two arrays: 
+        // The first array contains hashes, each of which contain an "id" key:
+        // [{"id": "chas"}, {"id":"frank"}]
+        //
+        // The second array consists of the id values you want to select
+        // records by.
+        // 
+        // Return an array containing only the hashes in the first array
+        // where a match was found.
+
+        // select_users = function(list, selections) {
+        //     return list.filter( function (a) {
+        //         for (var i = 0; i < selections.length; i++) {
+        //             if ( a['id'] == selections[i] ) return true;
+        //         }
+        //     });
+        // }
+
+        // list = [{"id": "chas"}, {"id":"frank"}];
+        // selections = ['chas'];
+        // select_users(list, selections);
+
+        select_users: function(list, selections) {
+            return list.filter( function (a) {
+                for (var i = 0; i < selections.length; i++) {
+                    if ( a['id'] == selections[i] ) return true;
+                }
+            });
         }
     }
 };
